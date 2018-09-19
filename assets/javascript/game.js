a6bf72498e92bfc8eae4f4f8f1c122c6a4b778b0
Mags <maggy123@yahoo.com>
2018-09-19 18:17:58 -0500

    Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,54 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
     "u", "v", "w", "x", "y", "z",];
 var wins = 0;
 var losses = 0;
-var numGuesses = 9;
+var numGuesses = 10;
 var guessedChoices = [];
 var userGuess;
 
+var reset = function () {
+    guessedChoices = [];
+    numGuesses = 10;
+}
+
 document.onkeydown = function (event) {
 
-    console.log(event.key);
+
     userGuess = event.key;
 
-    console.log(userGuess);
+    var randomcomputerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-var randomcomputerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(userGuess);
-if (userGuess === randomcomputerGuess) {
-    wins++;
-    console.log('wins:', wins);
-    numGuesses--;
-    console.log(numGuesses);
-    guessedChoices.push(userGuess);
-}
-else {
-    losses++;
-    console.log('losses:', losses);
-    numGuesses--;
-    guessedChoices.push(userGuess);
-}
+    if (userGuess === randomcomputerGuess) {
+        wins++;
+        numGuesses--;
+        guessedChoices.push(userGuess);
+    }
+    else {
+        losses++;
+        numGuesses--;
+        guessedChoices.push(userGuess);
+    }
 
-var html =
-    "<h1> The Psychic Game </h1>" +
-    "<p>Guess what letter I'm thinking of!</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses Left: " + numGuesses + "</p>" +
-    "<p>Your Guesses so far: " + guessedChoices.join(", ") + "</p>";
+    if (numGuesses === 0) {
+        reset();
+    }
 
-document.querySelector("#game").innerHTML = html;
+    
+
+    var html =
+        "<h1> The Psychic Game </h1>" +
+        "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + numGuesses + "</p>" +
+        "<p>Your Guesses so far: " + guessedChoices.join(", ") + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+
+
+
 };
+
+
 
 
 
